1b2aeeb991865e8ed5e1716ab00fddf1e932255a
Nih4rS <shivakumarnihar@gmail.com>
2025-03-29 04:59:07 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const heading = document.querySelector('h1');
+  heading.addEventListener('click', () => {
+    alert('Hello! Welcome to my portfolio.');
+  });
+});
